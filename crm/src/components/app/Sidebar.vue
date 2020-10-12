a2c0a7f3d4b9bb7c2f7dc7e995717b33046f2113
tsukiyyyomi <hunkonikita@gmail.com>
2020-10-12 23:28:58 +0300

    Created routes in Sidebar, fixed css

--- crm/src/components/app/Sidebar.vue
+++ crm/src/components/app/Sidebar.vue
@@ -1,19 +1,45 @@
 <template>
     <ul class="sidenav app-sidenav open">
-            <li>
-                <a href="#" class="waves-effect waves-orange pointer">Счет</a>
-            </li>
-            <li>
-                <a href="#" class="waves-effect waves-orange pointer">История</a>
-            </li>
-            <li>
-                <a href="#" class="waves-effect waves-orange pointer">Планирование</a>
-            </li>
-            <li>
-                <a href="#" class="waves-effect waves-orange pointer">Новая запись</a>
-            </li>
-            <li>
-                <a href="#" class="waves-effect waves-orange pointer">Категории</a>
-            </li>
+            <router-link 
+                v-for="link in links"
+                :key="link.url"
+                active-class="active"                
+                :to="link.url"     
+                tag="li"           
+                :exact="link.exact"
+                class="li-sidebar"
+            >
+                <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+            </router-link>
+
         </ul>
 </template>
+
+<script>
+    export default{
+        data: () => ({
+            links: [
+                {title: 'Счет', url: '/', exact:true},
+                {title: 'История', url: '/history'},
+                {title: 'Планирование', url: '/planning'},
+                {title: 'Новая запись', url: '/record'},
+                {title: 'Категории', url: '/categories'}
+            ]
+        })
+    }
+</script>
+
+<style lang="scss">
+    .li-sidebar{
+        display: flex;
+        color:#000;
+        margin: 1vw;
+        padding: 0.5vw 0.2vw;
+        a{
+            color:#000;
+        }
+    }
+    .active{
+        background: rgba(196, 196, 196, 0.425);
+    }
+</style>
