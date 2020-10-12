50ea5cf567d53821866f29edf557120397db57c7
tsukiyyyomi <hunkonikita@gmail.com>
2020-10-12 12:59:48 +0300

    Imported layouts and created loader for them

--- crm/src/App.vue
+++ crm/src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="app">
-      
-    <router-view />
+    <component :is="layout">
+      <router-view />
+    </component>
+    
   </div>
   
 </template>
+<script>
+import AuthLayout from '@/layouts/Auth.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+  export default{
+    computed: {
+      layout(){
+        console.log(this.$route.meta);
+        return (this.$route.meta.layout || 'auth') + '-layout';
+      }
+    },
+    components:{
+      AuthLayout, MainLayout
+    }
+  }
+</script>
 
 <style lang="scss">
   @import '~materialize-css/dist/css/materialize.min.css';
