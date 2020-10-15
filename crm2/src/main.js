f63b75c42959c016e37be1f15383a2e87df78888
tsukiyyyomi <hunkonikita@gmail.com>
2020-10-16 01:03:54 +0300

    Linked project to firebase

--- crm2/src/main.js
+++ crm2/src/main.js
@@ -7,9 +7,25 @@ import store from "./store";
 import messagePlagin from './utils/message.plagin';
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 Vue.config.productionTip = false;
 Vue.use(messagePlagin);
 Vue.use(Vuelidate);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVJGeUAq2icWV5Xf7_oy5pb24geo6HpbU",
+  authDomain: "vue-crm-tsukyyyomi.firebaseapp.com",
+  databaseURL: "https://vue-crm-tsukyyyomi.firebaseio.com",
+  projectId: "vue-crm-tsukyyyomi",
+  storageBucket: "vue-crm-tsukyyyomi.appspot.com",
+  messagingSenderId: "983510924369",
+  appId: "1:983510924369:web:b3eaaf6f4943853c370c70"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
